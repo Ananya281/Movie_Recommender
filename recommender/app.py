676308a6854a90c47bf3901958602d9c58899be3
Ananya <ananyachauhan647@gmail.com>
2025-04-11 19:46:53 +0530

    python

--- recommender/app.py
+++ recommender/app.py
@@ -227,4 +227,4 @@ def combined_movie_recommendation(query):
 # ===========================
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
